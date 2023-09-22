f42f94537c784f6349a2e07772fac662478ee76f
teepatra <fagboretomilola@gmail.com>
2023-09-22 14:04:19 -0700

    README.md file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# with the following requirements
+
+file { 'school':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
